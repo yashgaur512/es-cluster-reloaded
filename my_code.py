e2798fd3091176feb07b2df91cc416d818bd0085
yashgaur512 <yash.gaur.e@gmail.com>
2024-04-17 16:03:50 +0530

    updated my_code.py

--- my_code.py
+++ my_code.py
@@ -12,125 +12,6 @@ os.environ['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
 os.environ['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
 os.environ['AWS_SESSION_TOKEN'] = AWS_SESSION_TOKEN
 
-# class MyCode:
-#     def __init__(self):
-#         pass
-
-#     def _write_backend_file(self, file_path, old_word, new_word):
-#         try:
-#             # Read the content of the file
-#             with open(f"{file_path}/backend.tf", 'r') as file:
-#                 file_content = file.read()
-
-#             # Use regular expression to replace the word
-#             new_content = re.sub(r'\b' + re.escape(old_word) + r'\b', new_word, file_content)
-
-#             # Write the modified content back to the file
-#             with open(f"{file_path}/backend.tf", 'w') as file:
-#                 file.write(new_content)
-
-#             command = f"rm -rf {file_path}/.terraform {file_path}/.terraform.lock.hcl"
-#             os.system(command)
-#             print("Word replaced successfully and removed state files!")
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-
-#     def _init(self, file_path, old, new):
-#         try:
-#             if new != '':
-#                 self._write_backend_file(file_path, old, new)
-#                 self._destroy(file_path, old, new)
-#                 response = self._update_state({
-#                 "uuid": {"S": "ClusterFetchRecord"},
-#                 "active": {"S": old},
-#                 "new": {"S": ""}
-#                 })
-#             new = str(uuid.uuid4()) + '.tf'
-
-#             if old == '':
-#                 self._write_backend_file(file_path, 'dummy.tf', new)
-#             else:
-#                 self._write_backend_file(file_path, old, new)
-
-#             return new
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-
-#     def _destroy(self, file_path):
-#         try:
-#             tf = Terraform(working_dir=file_path)
-#             tf.init(reconfigure=IsNotFlagged)
-#             return_code, stderr = tf.destroy(force=IsNotFlagged, _auto_approve=IsFlagged)
-
-#             curr_state = self._fetch_state()
-
-#             if return_code == 0:
-#                 print("Terraform destroy successful 1!")
-#                 response = self._update_state({
-#                 "uuid": {"S": "ClusterFetchRecord"},
-#                 "active": {"S": curr_state['new']},
-#                 "new": {"S": ""}
-#                 })
-#             else:
-#                 print("Terraform destroy failed 1. See error message:")
-#                 print(stderr)
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-    
-#     def _apply(self, file_path, old, new):
-#         try: 
-#             new = self._init(file_path, old, new)
-#             tf = Terraform(working_dir=file_path)
-#             tf.init(reconfigure=IsNotFlagged)
-#             return_code, stderr = tf.apply(skip_plan=True)
-
-#             if return_code == 0:
-#                 print("Terraform Apply successful 1!")
-#                 return new
-#             else:
-#                 print("Terraform Apply failed 1. See error message:")
-#                 print(stderr)
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-        
-#     def cluster_create(self, file_path):
-#         try:
-#             curr_state = self._fetch_state()
-#             active = curr_state['active']
-#             new = curr_state['new']
-
-#             self._init(file_path, active, new)
-#             new = self._apply(file_path)
-
-#             self._update_state({
-#                 "uuid": {"S": "ClusterFetchRecord"},
-#                 "active": {"S": curr_state['active']},
-#                 "new": {"S": new}
-#                 })
-
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-    
-#     def cluster_swap(self, file_path):
-#         curr_state = self._fetch_state()
-#         new = curr_state['new']
-#         active = curr_state['active']
-#         try:
-#             if new == '':
-#                 print("No new cluster found")
-#                 return
-#             else:
-#                 self._write_backend_file(file_path, new, active)
-#                 self._destroy(file_path)
-#                 self._update_state({
-#                     "uuid": {"S": "ClusterFetchRecord"},
-#                     "active": {"S": new},
-#                     "new": {"S": ""}
-#                     })
-
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-
 def has_status_key(obj):
     return isinstance(obj, dict) and 'status' in obj.keys()
 
@@ -342,6 +223,6 @@ def destroy():
 
 # new = state_manager.get_new_state()
 # active = state_manager.get_active_state()
-# put_obj = build_object(new['statefile'], new['node0_ip'],  new['node1_ip'],  new['node2_ip'],  new['nlb_dns_name']) 
+# put_obj = build_object('statefile', 'node0_ip',  'node1_ip',  'node2_ip',  'nlb_dns_name') 
 
-# state_manager.update_state(put_obj, active)
+# state_manager.update_state(put_obj, put_obj)
